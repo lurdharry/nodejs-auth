fac030235a8230561e616cff4f6c17d53d296797
lurdharry <hammed@patricia.com.ng>
2020-06-08 15:36:43 +0100

    Sets up server entry point

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const UserRouter = require('./routes/userRoutes');
 
-// env data
+// env conection data from .env file
 const Mongoconnect = process.env.DB_CONNECT;
 const port = process.env.PORT;
 // mongo extra
@@ -12,16 +12,25 @@ const extra = {
   useUnifiedTopology: true,
 };
 
-// mongo server
-mongoose.connect(Mongoconnect, extra, () =>
-  console.log('connected to data base'),
-);
+// mongo cluster initialization
+mongoose
+  .connect(Mongoconnect, extra)
+  .then(() => {
+    console.log('Successfully connected to the database');
+  })
+  .catch((err) => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+  });
 
 const app = express();
-// app.use(db);
+// to convert response to json format
 app.use(express.json());
+
+//links  user routes  to server
 app.use(UserRouter);
 
+// to check if the server is  running
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
