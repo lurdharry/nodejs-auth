c69be7ae174ca617b9721e2ba185c56bb9151dea
lurdharry <hammed@patricia.com.ng>
2020-06-08 15:43:59 +0100

    Added controllers to users route to control what each route does

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const User = require('../models/User');
 const userController = require('../controllers/users');
+// const middleware = require('../middleware/verify');
 
 const router = express.Router();
 
 router.post('/users/register', userController.register);
 
-router.post('users/login', userController.login);
+router.post('/users/login', userController.login);
 
 module.exports = router;
