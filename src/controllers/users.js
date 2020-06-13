18d920628b33c053b4360b1b5f84c9972feda6b5
lurdharry <hammed@patricia.com.ng>
2020-06-13 19:50:15 +0100

    Implemented custom responseHandler in Auth Controllers

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,41 +1,54 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const responseHandler = require('../helpers/responsehandler');
 
+/**
+ * User registration.
+ *
+ * @param {String}    firstName
+ * @param {String}    lastName
+ * @param {String}    email
+ * @param {String}    password
+ * @param {String}    username
+ *
+ * @returns {Object}
+ */
 const register = (req, res) => {
   let emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   let passwordReg = new RegExp(
     '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})',
   );
   if (!req.body.firstname) {
-    res.status(400).send({
-      message: 'Firstname field cannot be empty',
-    });
+    return responseHandler.validationError(
+      res,
+      'Firstname field cannot be empty',
+    );
   }
   if (!req.body.lastname) {
-    res.status(400).send({
-      message: 'Lastname field cannot be empty',
-    });
+    return responseHandler.validationError(
+      res,
+      'Lastname field cannot be empty',
+    );
   } else if (!req.body.username) {
-    res.status(400).send({
-      message: 'Username field cannot be empty',
-    });
+    return responseHandler.validationError(
+      res,
+      'Username field cannot be empty',
+    );
   } else if (!req.body.password) {
-    res.status(400).send({
-      message: 'Password Field cannot be empty',
-    });
+    return responseHandler.validationError(
+      res,
+      'Password Field cannot be empty',
+    );
   } else if (!passwordReg.test(req.body.password)) {
-    res.status(400).send({
-      message:
-        'Password must be equal to or greater than 8 characters, must contain a lowercase letter at least, must contain an uppercase letter, .mustcontain a number, must contain a special character',
-    });
+    return responseHandler.validationError(
+      res,
+      'Password must be equal to or greater than 8 characters, must contain a lowercase letter at least, must contain an uppercase letter, .mustcontain a number, must contain a special character',
+    );
   } else if (!req.body.email) {
-    res.status(400).send({
-      message: 'Email Field cannot be empty',
-    });
+    return responseHandler.validationError(res, 'Email Field cannot be empty');
   } else if (!emailReg.test(req.body.email)) {
-    res.status(400).send({
-      message: 'Email is invalid',
-    });
+    return responseHandler.validationError(res, 'Email is invalid');
   }
   User.find(
     // check for two conditions before creating new user data
@@ -46,15 +59,9 @@ const register = (req, res) => {
       }
       if (doc.length) {
         if (doc[0].email === req.body.email) {
-          return res.status(409).send({
-            status: 409,
-            message: 'Email exists already',
-          });
+          return responseHandler.validationError(res, 'Email exists already');
         } else if (doc[0].username === req.body.username) {
-          return res.status(409).send({
-            status: 409,
-            message: 'Username already exist',
-          });
+          return responseHandler.validationError(res, 'Username already exist');
         }
       } else {
         let token = jwt.sign({ email: req.body.email }, process.env.JWT_KEY);
@@ -64,47 +71,43 @@ const register = (req, res) => {
           username: req.body.username,
           password: req.body.password,
           email: req.body.email,
-          token: token,
         });
         //save user in db
         user
           .save()
           .then((data) => {
-            res.status(200).send({
-              status: 200,
-              token: token,
-              message: 'Account created succesfully',
-              data: data,
-            });
+            responseHandler.loginSuccess(
+              res,
+              'Account created succesfully',
+              token,
+              data,
+            );
           })
           .catch((err) => {
-            res.status(500).send({
-              message:
-                err.message || 'Some error occurred while saving the User.',
-            });
+            responseHandler.errorResponse(
+              res,
+              err.message || 'Some error occurred while saving the User.',
+            );
           });
       }
     },
   );
 };
 
-// Login
-// Login
-const login = async (req, res) => {
+const login = (req, res) => {
   let regg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (
-    !req.body.password ||
-    req.body.password < 8 ||
-    !req.body.email ||
-    !regg.test(req.body.email)
-  ) {
-    return res.status(400).send({
-      message: 'Invalid Email Credentials',
-    });
+  if (!req.body.email || !regg.test(req.body.email)) {
+    return responseHandler.validationError(res, 'Invalid Email Credentials');
+  }
+  if (!req.body.password || req.body.password < 8) {
+    return responseHandler.validationError(
+      res,
+      'Your Password must more more than eight',
+    );
   }
 
   //find  users document
-  await User.findOne({ email: req.body.email }, (err, docs) => {
+  User.findOne({ email: req.body.email }, (err, docs) => {
     if (docs) {
       docs.comparePassword(req.body.password, (err, isMatch) => {
         if (isMatch) {
@@ -119,27 +122,105 @@ const login = async (req, res) => {
             },
           );
           // return the JWT token for the future API calls
-          res.status(200).send({
-            success: true,
-            message: 'Login successful!',
-            token: token,
-            data: docs,
-          });
+          responseHandler.loginSuccess(res, 'Login Successful!', token, docs);
         } else {
-          return res.status(400).send({
-            message: 'Invalid Credentials',
-          });
+          return responseHandler.validationError(
+            res,
+            'Your Paasword is inCorrect',
+          );
         }
       });
     } else {
-      return res.status(400).send({
-        message: 'Invalid Credentials',
-      });
+      return responseHandler.validationError(
+        res,
+        'Your Email is not registered',
+      );
     }
   });
+};
+
+const removeUser = (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return responseHandler.validationError(res, 'Invalid User ID');
+  }
+  User.findById(req.params.id, function (err, user) {
+    if (!user) {
+      return responseHandler.errorResponse(res, 'User details not found');
+    } else {
+      if (user._id.toString() !== req.params.id) {
+        return responseHandler.errorResponse(res, 'User id is not correct');
+      } else {
+        User.findByIdAndRemove(req.params.id, function (err, doc) {
+          if (err) {
+            return responseHandler.errorResponse(
+              res,
+              'Unable to delete  User at the moment pls try again',
+            );
+          } else {
+            return responseHandler.successResponse(
+              res,
+              `${doc.username} acoount deleted successfully`,
+            );
+          }
+        });
+      }
+    }
+  });
+};
+
+const updateProfile = (req, res) => {
+  if (
+    req.body.email ||
+    req.body.firstname ||
+    req.body.lastname ||
+    req.body.username
+  ) {
+    return res.status(403).send({
+      message: 'UnAuthorised user',
+    });
+  } else if (!req.body.username) {
+    return responseHandler.validationError(
+      res,
+      'Username field can not be empty',
+    );
+  } else {
+    Object.keys(req.body).forEach(function (key, index) {
+      User.findOneAndUpdate(
+        { username: req.body.username },
+        {
+          [key]: req.body[key],
+        },
+        { new: true },
+      )
+        .then((user) => {
+          if (!user) {
+            return responseHandler.validationError(
+              res,
+              `User not found with id ' ${req.body.username}`,
+            );
+          }
+          user.save();
+          res.send(user);
+        })
+        .catch((err) => {
+          if (err.kind === 'ObjectId') {
+            return responseHandler.validationError(
+              res,
+              'User not found with id ' + req.body.username,
+            );
+          }
+          return responseHandler.errorResponse(
+            res,
+            'Error updating user with id ' + req.body.username,
+          );
+        });
+    });
+  }
 };
 
 module.exports = {
   register: register,
   login: login,
+  updateDetails: updateProfile,
+  deleteUser: removeUser,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const userController = require('../controllers/users');
-// const middleware = require('../middleware/verify');
+const middleware = require('../middleware/verify');
 
 const router = express.Router();
 
 router.post('/users/register', userController.register);
 
 router.post('/users/login', userController.login);
+
+router.delete('/user/:id', userController.deleteUser);
+
+router.post(
+  'users/update',
+  middleware.verifyToken,
+  userController.updateDetails,
+);
 
 module.exports = router;
